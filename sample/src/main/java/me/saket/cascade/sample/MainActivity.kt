0e84ae0273c222e2ca00cdf8942df740ed316c58
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-10-18 20:12:05 +0530

    Added `overlayColor` in sample app

--- sample/src/main/java/me/saket/cascade/sample/MainActivity.kt
+++ sample/src/main/java/me/saket/cascade/sample/MainActivity.kt
@@ -104,6 +104,9 @@ class MainActivity : AppCompatActivity() {
       menuItem = {
         it.titleView.typeface = ResourcesCompat.getFont(this, R.font.work_sans_medium)
         it.itemView.background = rippleDrawable()
+      },
+      overlayColor = {
+        ContextCompat.getColor(this, R.color.window_overlay_background)
       }
     )
   }
