ee495c4afa6ff0b6b011aff9c289daa0c2142bfe
Saket Narayan <saket@squareup.com>
2021-01-30 21:23:52 -0500

    Revert "Prevent cancelation of ripples when a sub-menu is clicked"
    Turns out, ripples look weird for outgoing menus.
    
    This reverts commit 97c14e7e3054fd798fcd83d04f4edd45ab4055c4.

--- cascade/src/main/java/me/saket/cascade/ViewFlipper2.kt
+++ cascade/src/main/java/me/saket/cascade/ViewFlipper2.kt
@@ -1,66 +1,66 @@
 package me.saket.cascade
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
 import android.content.Context
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewPropertyAnimator
 import android.view.animation.Animation
-import android.widget.FrameLayout
 import android.widget.ViewFlipper
+import androidx.core.view.isVisible
 
 /**
- * Like [ViewFlipper], but uses [ViewPropertyAnimator] to animate child changes instead of
- * [Animation] which was causing [https://github.com/saket/cascade/issues/4].
- *
- * This does not extend [ViewFlipper] primarily because [ViewFlipper] toggles visibility of
- * children when [ViewFlipper.setDisplayedChild] is called resulting in ripples getting canceled.
+ * Like [ViewFlipper], but uses [ViewPropertyAnimator] to animate child changes
+ * instead of [Animation] which was causing [https://github.com/saket/cascade/issues/4].
  */
-abstract class ViewFlipper2 internal constructor(context: Context) : FrameLayout(context) {
-  internal var displayedChildView: View? = null
-    private set
+abstract class ViewFlipper2(context: Context) : ViewFlipper(context) {
+  val displayedChildView: View
+    get() = getChildAt(displayedChild)
 
-  internal fun setDisplayedChild(view: View) {
-    displayedChildView = view
-  }
-
-  internal fun setDisplayedChild(
+  fun setDisplayedChild(
     inView: View,
     inAnimator: (View) -> ViewPropertyAnimator,
     outAnimator: (View) -> ViewPropertyAnimator
   ) {
-    val outView = displayedChildView
-    displayedChildView = inView
+    val outView = getChildAt(displayedChild)
+    super.setDisplayedChild(indexOfChild(inView))
 
-    requireNotNull(outView)
-    inAnimator(inView).start()
-    outAnimator(outView).start()
-  }
-
-  // TODO: get rid of these 3 functions in favor of onViewRemoved() once minSdk > 23.
-  override fun removeAllViews() {
-    super.removeAllViews()
-    displayedChildView = null
-  }
-
-  override fun removeView(view: View) {
-    super.removeView(view)
-    if (displayedChildView == view) {
-      displayedChildView = null
+    if (outView != null) {
+      inAnimator(inView)
+        .setListener(
+          onStart = { inView.isVisible = true }
+        )
+        .start()
+      outAnimator(outView)
+        .setListener(
+          onStart = { outView.isVisible = true },
+          onEnd = { outView.isVisible = false }
+        )
+        .start()
     }
-  }
-
-  override fun removeViewAt(index: Int) {
-    getChildAt(index)?.let { removeView(it) }
   }
 
   override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
     // OG ViewFlipper animates view bitmaps instead of actual views,
     // where the displayed View is always at 0,0. This effectively means
     // that touch events are *always* received by the displayed child.
-    displayedChildView?.let {
+    return getChildAt(displayedChild).let {
       ev.offsetLocation(it.translationX, 0f)
-      return it.dispatchTouchEvent(ev)
+      it.dispatchTouchEvent(ev)
     }
-    return false
   }
+}
+
+private fun ViewPropertyAnimator.setListener(
+  onEnd: () -> Unit = {},
+  onStart: () -> Unit = {}
+): ViewPropertyAnimator {
+  setListener(object : AnimatorListener {
+    override fun onAnimationRepeat(animator: Animator) = Unit
+    override fun onAnimationCancel(animator: Animator) = Unit
+    override fun onAnimationEnd(animator: Animator) = onEnd()
+    override fun onAnimationStart(animator: Animator) = onStart()
+  })
+  return this
 }
