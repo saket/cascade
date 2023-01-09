8c100686f6b4085068adbcdc0e29e070042c452e
Saket <saket@squareup.com>
2023-01-08 22:54:19 -0500

    WIP: Try out a full-sized popup window with resizable composable

--- cascade-compose/src/main/java/me/saket/cascade/Cascade.kt
+++ cascade-compose/src/main/java/me/saket/cascade/Cascade.kt
@@ -2,8 +2,12 @@
 
 package me.saket.cascade
 
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
@@ -12,12 +16,16 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.LayoutScopeMarker
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowLeft
 import androidx.compose.material.icons.rounded.ArrowRight
@@ -34,19 +42,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntRect
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection.Ltr
 import androidx.compose.ui.unit.LayoutDirection.Rtl
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.lifecycle.ViewTreeViewModelStoreOwner
+import androidx.savedstate.findViewTreeSavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import me.saket.cascade.internal.DropdownMenuPositionProvider
 import me.saket.cascade.internal.cascadeTransitionSpec
+import java.util.UUID
+import kotlin.math.roundToInt
+
 
 /**
  * Material Design dropdown menu with support for nested menus.
@@ -88,20 +121,114 @@ fun CascadeDropdownMenu(
   state: CascadeState = rememberCascadeState(),
   content: @Composable CascadeColumnScope.() -> Unit
 ) {
-  DropdownMenu(
-    modifier = modifier,
-    expanded = expanded,
-    onDismissRequest = { onDismissRequest() },
-    offset = offset,
-    properties = properties,
-  ) {
-    CascadeDropdownMenuContent(
-      // A fixed width is needed because DropdownMenu
-      // does not handle width changes smoothly.
-      modifier = Modifier.requiredWidth(fixedWidth),
-      state = state,
-      content = content
+  //  DropdownMenu(
+  //    modifier = modifier,
+  //    expanded = expanded,
+  //    onDismissRequest = { onDismissRequest() },
+  //    offset = offset,
+  //    properties = properties,
+  //  ) {
+  //    CascadeDropdownMenuContent(
+  //      // A fixed width is needed because DropdownMenu
+  //      // does not handle width changes smoothly.
+  //      modifier = Modifier.requiredWidth(fixedWidth),
+  //      state = state,
+  //      content = content
+  //    )
+  //  }
+
+  val context = LocalContext.current
+  val density = LocalDensity.current
+  val hostView = LocalView.current
+  val layoutDirection = LocalLayoutDirection.current
+
+  val popupId = rememberSaveable { UUID.randomUUID() }
+  val popup = remember {
+    //PopupWindow(layout, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true)
+    ComposablePopupWindow(hostView, popupId)
+  }
+
+  var parentBounds by remember { mutableStateOf(IntRect.Zero) }
+
+  Box(
+    Modifier.onGloballyPositioned { coordinates ->
+      val parentCoordinates = coordinates.parentLayoutCoordinates!!
+      val layoutPosition = parentCoordinates.positionInWindow().let { offset ->
+        IntOffset(x = offset.x.roundToInt(), y = offset.y.roundToInt())
+      }
+      val layoutSize = parentCoordinates.size
+      parentBounds = IntRect(layoutPosition, layoutSize)
+    }
+  )
+
+  DisposableEffect(Unit) {
+    popup.width = ViewGroup.LayoutParams.MATCH_PARENT
+    popup.height = ViewGroup.LayoutParams.MATCH_PARENT
+    popup.contentView.background = null
+    popup.showAsDropDown(hostView, xoff = 0, yoff = 0, gravity = Gravity.NO_GRAVITY)
+
+    val popupPositionProvider = DropdownMenuPositionProvider(
+      contentOffset = offset,
+      density = density,
+      //onPositionCalculated = { parentBounds, menuBounds ->
+      //transformOriginState.value = calculateTransformOrigin(parentBounds, menuBounds)
+      //}
     )
+
+    popup.contentView.show(
+      ComposeView(context).apply {
+        setContent {
+          val windowSizeRectBuffer = remember { android.graphics.Rect(0, 0, 0, 0) }
+          var popupPosition by remember { mutableStateOf(IntOffset.Zero) }
+
+          CascadeDropdownMenuContent(
+            modifier = modifier
+              .fillMaxSize()
+              .padding(start = 4.dp, end = 4.dp, bottom = 4.dp)
+              .wrapContentWidth(align = Alignment.Start)  // Without this, requiredWidth() centers the content.
+              .requiredWidth(fixedWidth)
+              .wrapContentHeight(align = Alignment.Top)
+              .absoluteOffset { popupPosition }
+              .onSizeChanged {
+                val windowSize = windowSizeRectBuffer.let { rect ->
+                  hostView.getWindowVisibleDisplayFrame(windowSizeRectBuffer)
+                  IntSize(width = rect.width(), height = rect.height())
+                }
+
+                popupPosition = popupPositionProvider.calculatePosition(
+                  anchorBounds = parentBounds,
+                  windowSize = windowSize,
+                  layoutDirection = layoutDirection,
+                  popupContentSize = it,
+                )
+                println("Size changed to = $it. (parentBounds = $parentBounds, windowSize = $windowSize). calculated position = $popupPosition")
+              },
+            state = state,
+            content = content
+          )
+        }
+      },
+      forward = true
+    )
+
+    onDispose {
+      popup.dismiss()
+    }
+  }
+}
+
+private class ComposablePopupWindow(hostView: View, popupId: UUID) : CascadePopupWindow(hostView.context) {
+  init {
+    contentView.id = android.R.id.content
+    ViewTreeLifecycleOwner.set(contentView, ViewTreeLifecycleOwner.get(hostView))
+    ViewTreeViewModelStoreOwner.set(contentView, ViewTreeViewModelStoreOwner.get(hostView))
+    contentView.setViewTreeSavedStateRegistryOwner(hostView.findViewTreeSavedStateRegistryOwner())
+    // Set unique id for AbstractComposeView. This allows state restoration for the state
+    // defined inside the Popup via rememberSaveable()
+    contentView.setTag(R.id.compose_view_saveable_id_tag, "Popup:$popupId")
+
+    // Enable children to draw their shadow by not clipping them
+    contentView.clipChildren = false
   }
 }
 
---
+++ cascade-compose/src/main/java/me/saket/cascade/internal/DropdownMenuPositionProvider.kt
@@ -0,0 +1,110 @@
+package me.saket.cascade.internal
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntRect
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupPositionProvider
+import kotlin.math.max
+import kotlin.math.min
+
+@Immutable
+internal data class DropdownMenuPositionProvider(
+  private val contentOffset: DpOffset,
+  private val density: Density,
+  private val onPositionCalculated: (IntRect, IntRect) -> Unit = { _, _ -> }
+) : PopupPositionProvider {
+  override fun calculatePosition(
+    anchorBounds: IntRect,
+    windowSize: IntSize,
+    layoutDirection: LayoutDirection,
+    popupContentSize: IntSize
+  ): IntOffset {
+    // The min margin above and below the menu, relative to the screen.
+    val verticalMargin = with(density) { MenuVerticalMargin.roundToPx() }
+    // The content offset specified using the dropdown offset parameter.
+    val contentOffsetX = with(density) { contentOffset.x.roundToPx() }
+    val contentOffsetY = with(density) { contentOffset.y.roundToPx() }
+
+    // Compute horizontal position.
+    val toRight = anchorBounds.left + contentOffsetX
+    val toLeft = anchorBounds.right - contentOffsetX - popupContentSize.width
+    val toDisplayRight = windowSize.width - popupContentSize.width
+    val toDisplayLeft = 0
+    val x = if (layoutDirection == LayoutDirection.Ltr) {
+      sequenceOf(
+        toRight,
+        toLeft,
+        // If the anchor gets outside of the window on the left, we want to position
+        // toDisplayLeft for proximity to the anchor. Otherwise, toDisplayRight.
+        if (anchorBounds.left >= 0) toDisplayRight else toDisplayLeft
+      )
+    } else {
+      sequenceOf(
+        toLeft,
+        toRight,
+        // If the anchor gets outside of the window on the right, we want to position
+        // toDisplayRight for proximity to the anchor. Otherwise, toDisplayLeft.
+        if (anchorBounds.right <= windowSize.width) toDisplayLeft else toDisplayRight
+      )
+    }.firstOrNull {
+      it >= 0 && it + popupContentSize.width <= windowSize.width
+    } ?: toLeft
+
+    // Compute vertical position.
+    val toBottom = maxOf(anchorBounds.bottom + contentOffsetY, verticalMargin)
+    val toTop = anchorBounds.top - contentOffsetY - popupContentSize.height
+    val toCenter = anchorBounds.top - popupContentSize.height / 2
+    val toDisplayBottom = windowSize.height - popupContentSize.height - verticalMargin
+    val y = sequenceOf(toBottom, toTop, toCenter, toDisplayBottom).firstOrNull {
+      it >= verticalMargin &&
+        it + popupContentSize.height <= windowSize.height - verticalMargin
+    } ?: toTop
+
+    onPositionCalculated(
+      anchorBounds,
+      IntRect(x, y, x + popupContentSize.width, y + popupContentSize.height)
+    )
+    return IntOffset(x, y)
+  }
+}
+
+internal fun calculateTransformOrigin(
+  parentBounds: IntRect,
+  menuBounds: IntRect
+): TransformOrigin {
+  val pivotX = when {
+    menuBounds.left >= parentBounds.right -> 0f
+    menuBounds.right <= parentBounds.left -> 1f
+    menuBounds.width == 0 -> 0f
+    else -> {
+      val intersectionCenter =
+        (
+          max(parentBounds.left, menuBounds.left) +
+            min(parentBounds.right, menuBounds.right)
+          ) / 2
+      (intersectionCenter - menuBounds.left).toFloat() / menuBounds.width
+    }
+  }
+  val pivotY = when {
+    menuBounds.top >= parentBounds.bottom -> 0f
+    menuBounds.bottom <= parentBounds.top -> 1f
+    menuBounds.height == 0 -> 0f
+    else -> {
+      val intersectionCenter =
+        (
+          max(parentBounds.top, menuBounds.top) +
+            min(parentBounds.bottom, menuBounds.bottom)
+          ) / 2
+      (intersectionCenter - menuBounds.top).toFloat() / menuBounds.height
+    }
+  }
+  return TransformOrigin(pivotX, pivotY)
+}
+
+internal val MenuVerticalMargin = 48.dp
